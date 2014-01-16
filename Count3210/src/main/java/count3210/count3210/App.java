9be302365abf67eec538fd2da46811c6747f70f7
Tuukka Virtanen <duukkis@gmail.com>
2014-01-16 17:36:44 +0200

    projektityöhakemisto luotu

---
+++ Count3210/src/main/java/count3210/count3210/App.java
@@ -0,0 +1,13 @@
+package count3210.count3210;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
