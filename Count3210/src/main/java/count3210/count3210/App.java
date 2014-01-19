3d63f9695a5e12e12984536cf77b4994a88efcf9
Tuukka Virtanen <duukkis@gmail.com>
2014-01-19 20:08:04 +0200

    koodausta ja tuntikirjanpitoa

--- Count3210/src/main/java/count3210/count3210/App.java
+++ Count3210/src/main/java/count3210/count3210/App.java
@@ -1,13 +1,55 @@
 package count3210.count3210;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import count3210.count3210.domain.Laskuri;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        Laskuri sekunnit = new Laskuri(59);
+        Laskuri minuutit = new Laskuri(59);
+        Laskuri tunnit = new Laskuri(23);
+
+        System.out.print("sekunnit: ");
+        int sek = Integer.parseInt(lukija.nextLine());  // kysy sekuntien alkuarvo käyttäjältä
+        System.out.print("minuutit: ");
+        int min = Integer.parseInt(lukija.nextLine());  // kysy minuuttien alkuarvo käyttäjältä
+        System.out.print("tunnit: ");
+        int tun = Integer.parseInt(lukija.nextLine());  // kysy tuntien alkuarvo käyttäjältä
+
+        // Jokaiselle oliolle tulee omat oliomuuttuja-arvonsa,
+        // joita alla olevassa while-loopissa muutetaan metodin avulla.
+        sekunnit.asetaArvo(sek);
+        minuutit.asetaArvo(min);
+        tunnit.asetaArvo(tun);
+
+        aikaKulkee(tunnit, minuutit, sekunnit);
+
+    }
+
+    public static void aikaKulkee(Laskuri tunnit, Laskuri minuutit, Laskuri sekunnit) {
+        int i = 0;
+        while (i < 121) {
+            System.out.println(tunnit + ":" + minuutit + ":" + sekunnit);
+
+            sekunnit.seuraava();
+
+            if (sekunnit.arvo() == 0) {
+                minuutit.seuraava();
+
+                if (minuutit.arvo() == 0) {
+                    tunnit.seuraava();
+
+                    // Tämä suoritetaan vasta sitten, kun sekunnit, minuutit ja tunnit ovat nolla, eli vuorokausi vaihtuu.
+                    if (tunnit.arvo() == 0) {
+                        System.out.println(tunnit + ":" + minuutit + ":" + sekunnit);
+                        sekunnit.seuraava();
+                    }
+                }
+            }
+            i++;
+        }
     }
 }
---
+++ Count3210/src/main/java/count3210/count3210/domain/Laskuri.java
@@ -0,0 +1,54 @@
+
+package count3210.count3210.domain;
+
+
+public class Laskuri {
+    private int arvo;
+    private int ylaraja;
+
+    public Laskuri(int ylarajanAlkuarvo) {
+        this.arvo = 0;
+        this.ylaraja = ylarajanAlkuarvo;
+    }
+
+    public void seuraava() {
+        // Pitäisikö laskurin käyttää koko ajan laskurin
+        // ajan laskemiseen järjestelmän kelloa vai juoksisiko
+        // aika sekunti kerrallaan, kunnes käyttäjä sulkee
+        // ohjelman? Ohjelman uudelleen käynnistyksessä
+        // järjestelmän kellon arvo taas luettaisiin ja aika
+        // kulkisi sekunti kerrallaan.
+        
+        // Seuraavassa voisi vähentää this.arvo-muuttujan
+        // arvoa yhdellä, kunnes sen arvo on nolla.
+        
+        if (arvo <= ylaraja && arvo > 0) {
+            arvo--;
+        } else {
+            arvo = 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        
+        if (this.arvo < 10) {
+            return "0" + this.arvo;
+        } else {
+            return "" + this.arvo;
+        }
+    }
+    
+    public int arvo() {
+        // Tämä metodi ainoastaan palauttaa oliomuuttujan arvon,
+        // jotta sitä voi käyttää main-luokassa.
+        return this.arvo;
+    }
+        
+    public void asetaArvo(int asetus) {
+        // Tähän metodiin tulee tallennusluokan olion luonti ja
+        // sen metodien käyttöä. Asetukset tallentuvat taustalle
+        // ja näkyvä osa on lähtölaskenta, joka käyttää aloitus-
+        // ajan laskuun järjestelmän kellonaikaa ja päivämäärää.
+    }
+}
