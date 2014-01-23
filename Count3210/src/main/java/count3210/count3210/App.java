1c497a4efd6e76194c42f75f0a4267fc40f8bb05
Tuukka Virtanen <duukkis@gmail.com>
2014-01-23 16:59:43 +0200

    ohjelmalogiikan koodausta

--- Count3210/src/main/java/count3210/count3210/App.java
+++ Count3210/src/main/java/count3210/count3210/App.java
@@ -1,6 +1,8 @@
 package count3210.count3210;
 
 import count3210.count3210.domain.Laskuri;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
 public class App {
@@ -8,31 +10,61 @@ public class App {
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
 
-        Laskuri sekunnit = new Laskuri(59);
-        Laskuri minuutit = new Laskuri(59);
-        Laskuri tunnit = new Laskuri(23);
+//        Laskuri sekunnit = new Laskuri(59);
+//        Laskuri minuutit = new Laskuri(59);
+//        Laskuri tunnit = new Laskuri(23);
 
 //        int sek = 0;
 //        int min = 0;
 //        int tun = 3;
 
         // Tätä pitäisi muuttaa niin, että kysytään käyttäjältä päiväys.
-        System.out.print("sekunnit: ");
-        int sek = Integer.parseInt(lukija.nextLine());  // kysy sekuntien alkuarvo käyttäjältä
-        System.out.print("minuutit: ");
-        int min = Integer.parseInt(lukija.nextLine());  // kysy minuuttien alkuarvo käyttäjältä
-        System.out.print("tunnit: ");
-        int tun = Integer.parseInt(lukija.nextLine());  // kysy tuntien alkuarvo käyttäjältä
+//        System.out.print("sekunnit: ");
+//        int sek = Integer.parseInt(lukija.nextLine());  // kysy sekuntien alkuarvo käyttäjältä
+//        System.out.print("minuutit: ");
+//        int min = Integer.parseInt(lukija.nextLine());  // kysy minuuttien alkuarvo käyttäjältä
+//        System.out.print("tunnit: ");
+//        int tun = Integer.parseInt(lukija.nextLine());  // kysy tuntien alkuarvo käyttäjältä
+//        
+//        sekunnit.asetaArvo(sek);
+//        minuutit.asetaArvo(min);
+//        tunnit.asetaArvo(tun);
+//
+//        System.out.println(tunnit + ":" + minuutit + ":" + sekunnit);
+//        aikaKulkee(tunnit, minuutit, sekunnit);
         
-        sekunnit.asetaArvo(sek);
-        minuutit.asetaArvo(min);
-        tunnit.asetaArvo(tun);
+        // Kysy päiväyksen sekuntien, minuuttien, tuntien,
+        // vuorokausien, kuukausien ja vuosien arvo käyttäjältä.
+        System.out.print("sekunnit: ");
+        int sek = Integer.parseInt(lukija.nextLine());  
+        System.out.print("minuutit: ");
+        int min = Integer.parseInt(lukija.nextLine()); 
+        System.out.print("tunnit: ");
+        int tun = Integer.parseInt(lukija.nextLine());
+        System.out.println("vuorokaudet: ");
+        int vrk = Integer.parseInt(lukija.nextLine());
+        System.out.println("kuukaudet: ");
+        int kk = Integer.parseInt(lukija.nextLine());
+        System.out.println("vuodet: ");
+        int vv = Integer.parseInt(lukija.nextLine());
 
-        System.out.println(tunnit + ":" + minuutit + ":" + sekunnit);
-        aikaKulkee(tunnit, minuutit, sekunnit);
-
+        Calendar aikaNyt = Calendar.getInstance();
+        Calendar tapahtumanAika = Calendar.getInstance();
+        tapahtumanAika.set
+        
+//        aikaKulkeeCalendar();
     }
 
+    public static void aikaKulkeeCalendar() {
+        
+        Calendar aika = Calendar.getInstance();
+        System.out.println(aika.get(Calendar.DATE));
+        
+        aika.add(Calendar.DATE, 5);
+        System.out.println(aika.get(Calendar.DATE));
+        
+    }
+    
     public static void aikaKulkee(Laskuri tunnit, Laskuri minuutit, Laskuri sekunnit) {
         
         while (true) {
