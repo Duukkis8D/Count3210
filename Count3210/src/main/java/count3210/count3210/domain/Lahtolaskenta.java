7a3d68459997129296214b2a229a6ed11e24d0f8
Tuukka Virtanen <duukkis@gmail.com>
2014-02-25 14:37:10 +0200

    javadocin kirjoittamista

--- Count3210/src/main/java/count3210/count3210/domain/Lahtolaskenta.java
+++ Count3210/src/main/java/count3210/count3210/domain/Lahtolaskenta.java
@@ -7,6 +7,8 @@ import java.awt.event.ActionListener;
 import javax.swing.Timer;
 import org.joda.time.*;
 
+/** Luokka määrittelee, miten ohjelman ydintoiminto, lähtölaskenta, toimii.
+ */
 public class Lahtolaskenta implements ActionListener {
 
     private Period ajanjakso;
@@ -27,6 +29,9 @@ public class Lahtolaskenta implements ActionListener {
         this.ajastin = ajastin;
     }
 
+    /** Metodi huolehtii tapahtumaketjusta, joka alkaa lähtölaskennan
+     käynnistyessä.
+     */
     @Override
     public void actionPerformed(ActionEvent ae) {
 
@@ -41,6 +46,9 @@ public class Lahtolaskenta implements ActionListener {
         ui.paivitaTapahtumapaneeli();
     }
 
+    /** Metodi luo käyttäjän luoman tapahtuman ja nykyisen kalenteriajan välisen
+     erotuksen pituisen ajanjakson.
+     */
     public void ajanjaksonLuonti() {
         Tapahtuma tapahtuma = lahtolaskentaruutu.getTapahtuma();
         DateTime tapahtumaAika = tapahtuma.getTapahtumaAika();
@@ -49,10 +57,14 @@ public class Lahtolaskenta implements ActionListener {
         ajanjakso = new Period(aikaNyt, tapahtumaAika);
     }
 
+    /** Metodi päivittää lähtölaskentaruudun tapahtumaan jäljellä olevan ajan
+     * näyttävän tekstikentän.
+     * aikayksikkoja-muuttuja saa arvokseen ajanjakso-olion seuraavien
+     kenttien arvot: vuodet, kuukaudet, viikot, vuorokaudet, tunnit,
+     minuutit ja sekunnit.
+     @param aikayksikkoja
+     */
     public void lahtolaskentaruudunPaivitys(int aikayksikkoja) {
-        // aikayksikkoja-muuttuja saa arvokseen ajanjakso-olion seuraavien
-        // kenttien arvot: vuodet, kuukaudet, viikot, vuorokaudet, tunnit,
-        // minuutit ja sekunnit.
         aikayksikkoja = ajanjakso.getValue(0) + ajanjakso.getValue(1)
                 + ajanjakso.getValue(2) + ajanjakso.getValue(3)
                 + ajanjakso.getValue(4) + ajanjakso.getValue(5)
